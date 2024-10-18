c0c19142a71e3e659a521f87f1a60e6002b751b2
nourhanMohamed0 <nour.mohamed29999@gamil.com>
2024-10-18 04:55:24 +0300

    Dashboard Page

--- OrangeHRM/src/main/java/pages/DashboardPage.java
+++ OrangeHRM/src/main/java/pages/DashboardPage.java
@@ -1,4 +1,39 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class DashboardPage {
+    WebDriver dashBoardDriver;
+    public DashboardPage(WebDriver driver){
+        dashBoardDriver=driver;
+    }
+    By leaveRequests=By.xpath("//button[@title=\"Leave List\"]");
+    By pageTitle=By.xpath("//div[@class=\"oxd-topbar-header-title\"]");
+    By recruitmentApplication=By.xpath("//p[text()=\"My Actions\"]");
+    By performanceMetric=By.xpath("//span[text()='Performance']");
+    By dashboardTitle=By.xpath("//h6[text()='Dashboard']");
+
+    public void navigateToLeaveRequests(){
+        dashBoardDriver.findElement(leaveRequests).click();
+    }
+    public WebElement getDashboardTitle(){
+        return dashBoardDriver.findElement(dashboardTitle);
+    }
+    public String getCurrentPageTitle(){
+        return dashBoardDriver.findElement(pageTitle).getText();
+    }
+    public boolean isLeaveRequestsExist(){
+        return dashBoardDriver.findElement(leaveRequests).isDisplayed();
+    }
+    public boolean isRecruitmentApplicationExist(){
+        return dashBoardDriver.findElement(recruitmentApplication).isDisplayed();
+    }
+    public boolean isPerformanceMetricExist(){
+        return dashBoardDriver.findElement(performanceMetric).isDisplayed();
+    }
+
+
+
 }
